c41213156bba2ec8aaa13b9035e03b29df6d0288
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-04-08 14:50:24 +0900

    btn changhe

--- trackball_thumb/keymaps/default/keymap.c
+++ trackball_thumb/keymaps/default/keymap.c
@@ -21,8 +21,8 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_BTN5, 
+        KC_BTN6, 
         KC_BTN4, 
-        KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN6
+        KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN5
     ),
 };
